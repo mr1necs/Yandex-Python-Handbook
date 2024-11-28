3dde697d194dcbeb614773b32c0a9b5c6fb2d5e6
Maks <46063054+mr1necs@users.noreply.github.com>
2024-11-28 11:48:20 +0300

    Update stdin.py

--- stdin.py
+++ stdin.py
@@ -1,6 +1,12 @@
-from sys import stdin
-
-data = [line.strip("\n") for line in stdin]
-for line in data[:len(data) - 1]:
-    if line.lower().find(data[-1].lower()) != -1:
-        print(line)
+with (open(input(), 'r', encoding='utf-8') as file_in,
+      open(input(), 'w', encoding='utf-8') as file_even,
+      open(input(), 'w', encoding='utf-8') as file_odd,
+      open(input(), 'w', encoding='utf-8') as file_eq):
+    numbers = file_in.readline().split()
+    for number in numbers:
+        count = 0
+        for ch in number:
+            count += 1 if int(ch) > 0 else -1
+        if count > 0:
+            file_even.write(number)
+            
