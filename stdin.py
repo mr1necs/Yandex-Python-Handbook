5bc2ca6e8e50f50dbf42cde6570bba89b2254e77
Maks <46063054+mr1necs@users.noreply.github.com>
2024-11-29 14:42:23 +0300

    Update stdin.py

--- stdin.py
+++ stdin.py
@@ -1,12 +1,13 @@
-with (open(input(), 'r', encoding='utf-8') as file_in,
-      open(input(), 'w', encoding='utf-8') as file_even,
-      open(input(), 'w', encoding='utf-8') as file_odd,
-      open(input(), 'w', encoding='utf-8') as file_eq):
-    numbers = file_in.readline().split()
-    for number in numbers:
-        count = 0
-        for ch in number:
-            count += 1 if int(ch) > 0 else -1
-        if count > 0:
-            file_even.write(number)
-            
+import json
+from sys import stdin
+
+json_file = input()
+with open(json_file, encoding="UTF-8") as file_in:
+    records = json.load(file_in)
+
+for in_data in stdin:
+    name, value = in_data.rstrip("\n").split(" == ")
+    records[name] = value
+
+with open(json_file, "w", encoding="UTF-8") as file_out:
+    json.dump(records, file_out, ensure_ascii=False, indent=2)
